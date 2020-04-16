db007d0685bed3fc5697f834bf3e93a7385c03f4
MORENO ROJAS John Jairo <johnmorenoing@gmail.com>
2020-04-16 21:20:20 +0200

    setup project init

---
+++ src/main/java/com/example/base/repository/spring/cloud/BaseRepositorySpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.base.repository.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseRepositorySpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseRepositorySpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/base/repository/spring/cloud/BaseRepositorySpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.base.repository.spring.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseRepositorySpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
